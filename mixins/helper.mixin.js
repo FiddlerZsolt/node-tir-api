af33f55f6c9a785df8164845da61c49a3f93be2b
Zsolt Fiddler <hegedus.zsolt@codeyard.eu>
2023-10-03 19:08:40 +0200

    create logger and error handler helper methods

--- mixins/helper.mixin.js
+++ mixins/helper.mixin.js
@@ -3,5 +3,17 @@
 module.exports = {
 	name: "helper",
 
-	methods: {},
+	methods: {
+		// logMessage(message) {
+		// 	this.broker.logger.info(`>>>>>>>>> [${this.name}] ${message}`);
+		// },
+		logMessage(message) {
+			console.log(`\n>>>>>>>>> [${this.name}] ${message}`);
+		},
+		errorHandler(error) {
+			console.log("\n>>>>>>>>> [ERROR]");
+			console.log(error);
+			throw error;
+		},
+	},
 };
